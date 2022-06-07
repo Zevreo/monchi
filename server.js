99d45896cb03735f3ed27bfe1123dc9578cc6eca
Zevreo <99522923+Zevreo@users.noreply.github.com>
2022-06-07 00:17:36 -0500

    Initial commit w/ server and mongoose

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require("path");
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(fileUpload());
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () =>{
+    console.log('MongoDB se ha conectado');
+});
+
+app.listen(port, () =>{
+    console.log(`Server is running on port: ${port}`);
+});
