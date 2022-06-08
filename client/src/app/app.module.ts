8a54346bab1d842fe3b806e22c25d2d917460ff8
Zevreo <99522923+Zevreo@users.noreply.github.com>
2022-06-08 01:40:34 -0500

    Change title Angular

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +12,11 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    Title
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
