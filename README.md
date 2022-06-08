8a4fdf881faab439e4949a6829c871b57cd0db45
Zevreo <99522923+Zevreo@users.noreply.github.com>
2022-06-08 00:04:31 -0500

    Auth Test

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# monchi
+# MonChi
+## Documentación útil
+```
 > https://angular.io/tutorial
 > https://www.npmjs.com/package/mongoose
+> 
+> 
+```
 ## Configuración inicial
 1. *npm i* en la raíz del proyecto para instalar las dependecias del servidor.
 2. *npm install -g @angular/cli* para instalar el CLI de Angular.
--- routes/users.js
+++ routes/users.js
@@ -41,20 +41,37 @@ router.route('/:id').get((req, res) => {
     .catch(err => res.status(400).json('Error: '+err));
 });
 
+//POST Auth Test
+router.post('/auth', (req, res) =>{
+    const { EmailAddress, Password } = req.body;
+    if( !EmailAddress || !Password ) {
+        return res.status(400).json('Introduzca todos los campos');
+    }
+    User.findOne({ EmailAddress })
+    .then(user => {
+        if(!user) return res.status(400).json('No se encontro el usuario');
+        bcrypt.compare(Password, user.Password)
+        .then(isMatch => {
+            if(!isMatch) return res.status(400).json('Contraseña incorrecta');
+            res.json('Todo correcto')
+        })
+    })
+});
+
 //PUT Update ById
 router.route('/:id').put((req, res) => {
     User.findById(req.params.id)
     .then(user => {
-        user.FirstName = req.body.FirstName;
-        user.LastName = req.body.LastName;
-        user.EmailAddress = req.body.EmailAddress;
-        user.BirthDate = req.body.BirthDate;
-        user.Password = bcrypt.hashSync(req.body.Password);
-        user.Country = req.body.Country;
-        user.PhoneNumber = req.body.PhoneNumber;
-        user.DefaultCoin = req.body.DefaultCoin;
-        user.Role = req.body.Role;
-        user.Status = req.body.Status;
+        user.FirstName = (req.body.FirstName ? req.body.FirstName : user.FirstName);
+        user.LastName = (req.body.LastName ? req.body.LastName : user.LastName);
+        user.EmailAddress = (req.body.EmailAddress ? req.body.EmailAddress : user.EmailAddress);
+        user.BirthDate = (req.body.BirthDate ? req.body.BirthDate : user.BirthDate);
+        user.Password = (req.body.Password ? bcrypt.hashSync(req.body.Password) : user.Password);
+        user.Country = (req.body.Country ? req.body.Country : user.Country);
+        user.PhoneNumber = (req.body.PhoneNumber ? req.body.PhoneNumber : user.PhoneNumber);
+        user.DefaultCoin = (req.body.DefaultCoin ? req.body.DefaultCoin : user.DefaultCoin);
+        user.Role = (req.body.Role ? req.body.Role : user.Role);
+        user.Status = (req.body.Status ? req.body.Status : user.Status);
 
         user.save()
         .then(() => res.json('Usuario actualizado'))
