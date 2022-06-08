ee10f3f8406b51f4921e88be81481ee28e57b65f
Zevreo <99522923+Zevreo@users.noreply.github.com>
2022-06-08 16:39:50 -0500

    Conditional Auth
    Para modificar usuarios se requerirá tener la misma ID o ser un administrador.

---
+++ routes/auth.js
@@ -0,0 +1,45 @@
+const router = require('express').Router();
+let User = require('../models/users.model');
+const jwt = require('jsonwebtoken');
+const config = require('../config/default');
+const bcrypt = require('bcryptjs');
+const auth = require('../middleware/auth')
+
+router.post('/', (req, res) =>{
+    const { EmailAddress, Password } = req.body;
+    if( !EmailAddress || !Password ) {
+        return res.status(400).json('Introduzca todos los campos');
+    }
+    User.findOne({ EmailAddress })
+    .then(user => {
+        if(!user) return res.status(400).json('No se encontro el usuario');
+        bcrypt.compare(Password, user.Password)
+        .then(isMatch => {
+            if(!isMatch) return res.status(400).json('Contrasena incorrecta');
+            jwt.sign(
+                { id: user.id, Role: user.Role },
+                config.jwtSecret,
+                { expiresIn: 86400 },
+                (err, token) => {
+                    if(err) throw err;
+                    res.json({
+                        token,
+                        user: {
+                            id:user.id,
+                            Role:user.Role
+                        }
+                    })
+                }
+            )
+        })
+    })
+});
+
+router.get('/user', auth, (req, res) => {
+    User.findById(req.user.id)
+    .select('-password')
+    .then(user => res.json(user))
+    .catch(err => res.status(400).json('Error: '+err));
+});
+
+module.exports = router;
