11c950409615815538df12a5c9f714b8f2c32354
CassandraTello <74328624+CassandraTello@users.noreply.github.com>
2022-06-13 02:22:16 -0500

    Car
    car

---
+++ client/src/reducers/shoppingreducer.js
@@ -0,0 +1,18 @@
+
+export const shoppinginitialstate = {
+    products: [
+      {id:1, name: "p1", precio: 100},
+      {id:2, name: "p1", precio: 100},
+      {id:3, name: "p1", precio: 100},
+      {id:4, name: "p1", precio: 100},
+      {id:5, name: "p1", precio: 100},
+      {id:6, name: "p1", precio: 100},
+      {id:7, name: "p1", precio: 100},
+      {id:8, name: "p1", precio: 100},
+      {id:9, name: "p1", precio: 100},
+      {id:10, name: "p1", precio: 100},
+      {id:11, name: "p1", precio: 100},
+      {id:12, name: "p1", precio: 100},
+
+    ]
+}
