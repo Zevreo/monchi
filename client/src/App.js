d1839f108c936c1a0d5238aa0027dd030c6ba7cb
gabrielFierro0 <19393097@utcancun.edu.mx>
2022-06-12 21:36:49 -0500

    product

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import 'react-devtools';
+import "./index.css"
+import "./App.css"
+import products from "./assets/products.json"
+import Product from "./components/Product";
+
 
 //Redux
 import { loadUser } from './actions/authActions';
@@ -12,7 +17,7 @@ import Navigation from './components/navigation';
 import Login from './components/login';
 import Register from './components/register';
 
-export class App extends Component {
+/*export class App extends Component {
   componentDidMount() {
     store.dispatch(loadUser());
   }
@@ -31,6 +36,27 @@ export class App extends Component {
       </Router>
     );
   };
-};
+};*/
+//export default App;
+export default function App() {
+  return (
+      <div className={"container"}>
+        <main className={"main"}>
+          <h1>
+            monchi
+          </h1>
 
-export default App;
+          <div className={"grid"}>
+            {
+              products.map((product, i) => <Product {...product} key={i}/>)
+            }
+          </div>
+        </main>
+        <div
+            id="snipcart"
+            data-api-key="NWMwZWNkZGMtZjU2ZS00YzM3LWFlZjYtMmM5Zjk0MWViZDcxNjM3Njg0OTY0ODg5NTk4MTM3" hidden
+        >
+        </div>
+      </div>
+  );
+};
