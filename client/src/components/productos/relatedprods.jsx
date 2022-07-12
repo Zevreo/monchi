9b402a7f2e7dfc732cfce30716c628696777930c
Johan9565 <83249537+Johan9565@users.noreply.github.com>
2022-07-11 21:24:49 -0500

    related-idontknowwhatididherexd

--- client/src/components/productos/relatedprods.jsx
+++ client/src/components/productos/relatedprods.jsx
@@ -15,32 +15,66 @@ const responsive = {
     1024: { items: 3 }
 };
 
-const createItems = (length, [handleClick]) => {
+const createItems = (length, [handleClick],products) => {
 
 
     return Array.from({ length }).map((item, i) => (
         <div
             className="item"
-          
+
         >
-             <img src="https://img.fotocommunity.com/atardecer-d96880eb-c839-482f-a209-70fe2cc8ff4d.jpg?height=1080" class="carouselimage" alt="a"/>
+            <div>
+            {products? products.map((d,i)=>
+                        <img src={d.ProductImages[0]} class="carouselimage" alt="a"/>
+
+            ):"loading"}
+
+            </div>
+            
         </div>
     ));
 };
-export function Gallery (props) {
+
+export function Gallery(props) {
     const [products, setProducts] = useState(null);
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [items] = useState(createItems(products, [setActiveIndex]));
+        const [activeIndex, setActiveIndex] = useState(0);
+        const items = items = useState(createItems(products.length, [setActiveIndex],products));
+   
+    if (Array.isArray(products) && products.length > 0) {
+       
+        // items.push(
+
+        // <div>
+        //     |
+        //     {products ? products.map((d, i) =>
+
+        //         <div class="item">
+        //             <img src={d.ProductImages[0]} class="contain" alt="a" />
+        //         </div>
+
+
+        //     ) : "Loading"}
+
+        //     <p>
+        //         {products ? products.length : 0}
+        //     </p>
+        // </div>
+        // )
+    }
+
+
+
+
     console.log(products)
     async function getProductsByPrice() {
-        const{precio}=props;
+        const { precio } = props;
         await axios.get(`/api/product/searchbyprecio/${precio}`)
-         .then(prod=>setProducts(prod.data));
+            .then(prod => setProducts(prod.data));
     }
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         getProductsByPrice();
-    },[]) 
+    }, [])
 
     return [
         <AliceCarousel
