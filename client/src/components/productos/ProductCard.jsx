9a0573bf3fe251f4b48605b3ed61cf919b525961
gabrielFierro0 <19393097@utcancun.edu.mx>
2022-07-14 20:44:25 -0500

    paypal

--- client/src/components/productos/ProductCard.jsx
+++ client/src/components/productos/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = props => {
     const { Product, Key } = props;
     const { user } = props.auth;
     return (
-        <li class="relative col-lg-3 col-md-4 col-sm-6" style={{ padding: '15px' }} key={Key}>
+        <li class="relative col-lg-3 col-md-4 col-sm-6" style={{ padding: '25px' }} key={Key}>
             <Link to={`/product/${Product._id}`}>
                 <div class="item">
                     <img src={Product.ProductImages[0]} alt="#" class="contain" />
@@ -22,10 +22,13 @@ const ProductCard = props => {
                     </div>
                 </div>
             </Link>
+            aqui
         </li>
     )
 }
+
 const mapStateToProps = (state) => ({
     auth: state.auth
 });
-export default connect(mapStateToProps, null)(ProductCard);
+
+export default connect(mapStateToProps, null)(ProductCard, Loader);
