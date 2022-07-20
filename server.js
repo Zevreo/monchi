91ec0620761f626cf0cf8aac1b05c7b96cb8920e
Zevreo <99522923+Zevreo@users.noreply.github.com>
2022-07-19 23:48:21 -0500

    b

--- server.js
+++ server.js
@@ -12,18 +12,13 @@ const port = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
-app.use(express.static(path.join(__dirname, "client", "build","public")))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const connection = mongoose.connection;
 connection.once('open', () =>{
     console.log('MongoDB se ha conectado');
-});
-
-
-app.listen(port, () =>{
-    console.log(`Server is running on port: ${port}`);
 });
 
 // Routes
@@ -35,3 +30,12 @@ app.use('/api/product', require('./routes/products'));
 app.use('/api/fixed', require('./routes/fixed'));
 app.use('/api/cart', require('./routes/cart'));
 app.use('/api/order', require('./routes/orders'));
+
+//Make Express serve Client
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, () =>{
+    console.log(`Server is running on port: ${port}`);
+});
