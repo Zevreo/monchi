1bfc9eef3a276455aef39770f2039f6293b8016e
Johan9565 <83249537+Johan9565@users.noreply.github.com>
2022-07-23 01:34:21 -0500

    awdasdasd

--- client/src/components/user/shoppingcart.jsx
+++ client/src/components/user/shoppingcart.jsx
@@ -15,7 +15,6 @@ export function ShoppingCart(props) {
   const [Address, setAddress] = useState();
   const [Shipment, setShipment] = useState();
   const [Carrier, setCarrier] = useState();
-  const [Carriercode, setCarriercode] = useState();
 
   useEffect(() => {
     GetCart();
@@ -114,17 +113,7 @@ export function ShoppingCart(props) {
   useEffect(() => {
     CalcTotal();
   }, [Products]);
-useEffect(()=>{
-if(Carrier==="ups"){
-  setCarriercode("07")
-}
-if(Carrier==="dhl"){
-  setCarriercode("8")
-}
-else{
-  setCarriercode("2")
-}
-},[Carrier])
+
   async function CalcTotal() {
     let conv = 0;
     let Subtotal = 0;
@@ -250,21 +239,13 @@ else{
                           class="bg-white half-left"
                           type="text"
                           placeholder="Status"
-                          onChange={(e) => setCarrier(e.target.value)}
+                          onChange={e=>setCarrier(e.target.value)}
                         >
+                              
                           <option default>elige la opcion</option>
-                          <option value="ups">
-                            UPS-{Shipment.UPS[0].currency}$
-                            {Shipment.UPS[0].total_amount}
-                          </option>
-                          <option value="dhl">
-                          dhl-{Shipment.dhl[0].currency}$
-                            {Shipment.dhl[0].total_amount}
-                          </option>
-                          <option value="redpack">
-                            Redpack-{Shipment.Redpack[0].currency}$
-                            {Shipment.Redpack[0].total_amount}
-                          </option>
+                          <option value='{carrier:"UPS", carriercode:"07"}'>UPS-{Shipment.UPS[0].currency}${Shipment.UPS[0].total_amount}</option>
+                          <option value='{carrier:"Estafeta", carriercode:"07"}'>Estafeta-{Shipment.Estafeta[0].currency}${Shipment.Estafeta[0].total_amount}</option>
+                          <option value="Removed">Removido</option>
                         </select>
                       )}
                     </td>
